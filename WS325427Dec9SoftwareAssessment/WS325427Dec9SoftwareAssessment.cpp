4f7c58a831848128568250078e0376e18514d0a4
Joshua Moron <joshua.moron@burohappold.com>
2020-12-06 00:04:52 +0000

    Starting Templates

---
+++ WS325427Dec9SoftwareAssessment/WS325427Dec9SoftwareAssessment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <string.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
